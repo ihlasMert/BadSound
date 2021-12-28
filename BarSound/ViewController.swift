510858146b06a0358aac44494141e94f0627e7b0
İhlas Mert <ihlas1mert@gmail.com>
2021-12-28 21:44:56 +0300

    user interface soundBad

---
+++ BarSound/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BarSound
+//
+//  Created by ihlas on 28.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func aTapped(_ sender: Any) {
+    }
+    
+    @IBAction func bTapped(_ sender: Any) {
+    }
+    @IBAction func cTapped(_ sender: Any) {
+    }
+    
+    @IBAction func dTapped(_ sender: Any) {
+    }
+    
+}
+
